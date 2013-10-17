ae1714a3dc93e4194ba77fbb55c2ef2781e16ebe
Roger Gee <rpg11a@acu.edu>
2013-10-17 09:33:05 -0500

    Added build script files

---
+++ code/build.bat
@@ -0,0 +1,4 @@
+:: This file is owned by Roger. It will compile the AdventureGame project for
+:: users using Windows console (Command Prompt)
+
+echo Hello World
---
+++ code/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This file is owned by Roger. It will compile the AdventureGame project if you
+# are using a bash shell.
+
+echo Hello World
