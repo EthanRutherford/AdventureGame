518903754ca1800847fb4034a189b837aee4f21c
Ethan Waddle <ethan@Inspiron-1545.(none)>
2013-10-21 19:21:42 -0500

    there are keys, disposables, and treasures

--- Item.h
+++ Item.h
@@ -1,7 +1,9 @@
 //Ethan W. Design Document
 #include <string> // get 'string' type
 
-enum Effect {INVISIBLE, FIREPROOF, CURED, QUENCHED, LIT}; //This is an example of an enumeration, for disposables, if we so choose to implement disposables that way.  For example, water's effect is QUENCHED, Invisibility Potion's effect is INVISIBLE
+enum Items {Key, Treasure, Potion}; //This is an example of an enumeration, for disposables, if we so choose to implement disposables that way.  For example, water's effect is QUENCHED, Invisibility Potion's effect is INVISIBLE
+
+// An ITEM is something that can be taken.
 
 class Item
 {
@@ -10,18 +12,22 @@ class Item
 	Item();
 	~Item();
 	
-	virtual bool can_take_()const
-	{return true;}	
 
-	void setName(String NewX){Name=NewX;}
+	//void setName(String NewX){Name=NewX;}
 	
 	String getName()const{return Name;}
+	int getID{return ID;}
+	bool isConsumable{return consumable;}
 	
 	virtual void useItem();// May or may not implement item usage within to 
 
 private:
-	
+`	
 	String Name;
+	int ID;
+	bool consumable; //if it is consumable, we return -1 if it's already used
+			 //this means that if an item is not disposable, we return true
+			 //if an item is disposable and we have an unused item in our inventory, we return true
 	
 	};
 
@@ -31,9 +37,10 @@ private:
 class Disposable: public Item //potions, etc.
 {
  public:
-			
-	Effect getEffect()const {return x}
-	void setEffect(Effect newEff) {x=newEff}
+	
+	//Ignore Effect for the sake of the demo		
+	// Effect getEffect()const {return x}
+	// void setEffect(Effect newEff) {x=newEff}
 	
 	
 	void useItem();
@@ -44,7 +51,7 @@ class Disposable: public Item //potions, etc.
  private:
 	bool Status; //can be initialized to true, meaning can be used.  For example, a potion in inventory that has not been drunk will be true.
 	
-	Effect x;
+	//Effect x;
  };
 
 
