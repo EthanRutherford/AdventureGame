3dc69788b4110561f7a57fb7b681f2c1a741f6ba
Ethan Rutherford <ethanrutherford2@yahoo.com>
2013-10-30 19:50:24 -0500

    Ethanr: made the change to compare_name

--- code/game_element.cpp
+++ code/game_element.cpp
@@ -4,9 +4,12 @@ using namespace adventure_game;
 
 bool game_element::compare_name(const string& elementName) const
 {
-    string lowerName = name;
+    string lowerName = name, lowerElem = elementName;
     for (size_t i = 0;i<lowerName.length();i++)
         if (lowerName[i]>='A' && lowerName[i]<='Z')
             lowerName[i] -= 'A', lowerName[i] += 'a';
-    return elementName == lowerName;
+    for (size_t i = 0;i<lowerElem.length();i++)
+        if (lowerName[i]>='A' && lowerElem[i]<='Z')
+            lowerName[i] -= 'A', lowerElem[i] += 'a';
+    return lowerElem == lowerName;
 }
